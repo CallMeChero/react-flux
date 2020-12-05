9e167f746515b742002892bd02edccdb126f2f98
CallMeChero <david.ceranic@gmail.com>
2020-12-05 16:07:15 +0100

    Implement update course from flux store

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -7,7 +7,9 @@ export function saveCourse(course) {
     return courseApi.saveCourse(course).then(savedCourse => {
         dispatcher.dispatch({
             // Action
-            actionType: actionTypes.CREATE_COURSE,
+            actionType: course.id 
+            ? actionTypes.UPDATE_COURSE 
+            : actionTypes.CREATE_COURSE,
             course: savedCourse
         });
     })
